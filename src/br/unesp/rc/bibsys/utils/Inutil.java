ae3cbdd5f72971d6d6f89f54657290b2507f6c16
Artur <tuifm@hotmail.com>
2017-05-08 22:49:38 -0300

    Povoando pacotes. OBS: mudar depois

---
+++ src/br/unesp/rc/bibsys/utils/Inutil.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.rc.bibsys.utils;
+
+/**
+ *
+ * @author tuifm
+ */
+public class Inutil
+{
+    
+}
