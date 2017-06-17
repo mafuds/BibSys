1995865f642db8d3ea667b5abe56e09d0504f4dc
mariana <marifant26@gmail.com>
2017-06-17 12:31:24 -0300

    Comecei o hard coding

--- src/br/unesp/rc/bibsys/utils/Conversor.java
+++ src/br/unesp/rc/bibsys/utils/Conversor.java
@@ -41,68 +41,84 @@ public class Conversor
         }
         return conteudo.toString();
     }
-    
-    static public File converte(File arquivo) {
-        System.out.println("entrou no converte");
-        File teste = null;
-        HashMap<String, String> hm = new HashMap<>();
-        Elemento e = new Elemento();
-        String patternRef = "[@](\\w+)(\\s*)[{]";
-        String patternAtr = "(\\w+)\\s*[=]\\s*(.+)";
-        
-        // Create a Pattern object
-        Pattern referenciaP = Pattern.compile(patternRef);
-        Pattern atributosP = Pattern.compile(patternAtr);
-        Matcher referencia = null;
-        Matcher atributos = null;
 
-        //String texto = Conversor.lerArquivo(arquivo);
+    static public File converte(File arquivo) {
+        HashMap<String, String> hm = new HashMap<>();
         
         try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
-            String s;
-            while ((s = reader.readLine()) != null) {
-                System.out.println(s);
-                
-                // verifica se é linha de referencia (@article, por ex)
-                referencia = referenciaP.matcher(s);
-                if (referencia.find()) {
-                    e.setReferencia(referencia.group(1));
-                }
-                
-                // verifica se é linha de atributos (year = {2012}, por ex)
-                atributos = atributosP.matcher(s);
-                if (atributos.find()) {
-                    hm.put(atributos.group(1), atributos.group(2));
-                    e.setNomeTag(atributos.group(1));   // nome do atributo
-                    e.setContTag(atributos.group(2));   // valor do atributo
-//                    e.setContTag(atributos.group(4));
-                    System.out.println("bateu");
-                    System.out.println("qtde grupos: " + atributos.groupCount());
-                    for (int i = 0; i <= atributos.groupCount(); i++) {
-                        System.out.println("atributos("+i+"): " + atributos.group(i) );
-                    }
-                    if (atributos.group(1).equals("year")) {
-                        Pattern p = Pattern.compile("[{]?(\\d+)[}]?[,]?");
-                        
-                        Matcher m = p.matcher(atributos.group(2));
-                        System.out.println("qtde grupos: " + m.groupCount());
-                        for (int i = 0; i <= m.groupCount(); i++) {
-                            System.out.println("m("+i+"): " + m.group(i) );
-                        }
-                        
-//                        System.out.println("Achou: " + m.group(0));
-//                        e.setAno(atributos.group(2));
-                    }
-                }
+            char s;
+            while ((s = (char) reader.read()) != -1) {
                 
             }
-            
         }   catch (IOException ex) {
             System.out.println("Ocorreu um erro ao ler o arquivo! " + ex.getMessage());
         }
+
         
-        
-        
-        return teste;
+        return arquivo;
     }
+    
+//    static public File converte(File arquivo) {
+//        System.out.println("entrou no converte");
+//        File teste = null;
+//        HashMap<String, String> hm = new HashMap<>();
+//        Elemento e = new Elemento();
+//        String patternRef = "[@](\\w+)(\\s*)[{]";
+//        String patternAtr = "(\\w+)\\s*[=]\\s*(.+)";
+//        
+//        // Create a Pattern object
+//        Pattern referenciaP = Pattern.compile(patternRef);
+//        Pattern atributosP = Pattern.compile(patternAtr);
+//        Matcher referencia = null;
+//        Matcher atributos = null;
+//
+//        //String texto = Conversor.lerArquivo(arquivo);
+//        
+//        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+//            String s;
+//            while ((s = reader.readLine()) != null) {
+//                System.out.println(s);
+//                
+//                // verifica se é linha de referencia (@article, por ex)
+//                referencia = referenciaP.matcher(s);
+//                if (referencia.find()) {
+//                    e.setReferencia(referencia.group(1));
+//                }
+//                
+//                // verifica se é linha de atributos (year = {2012}, por ex)
+//                atributos = atributosP.matcher(s);
+//                if (atributos.find()) {
+//                    hm.put(atributos.group(1), atributos.group(2));
+//                    e.setNomeTag(atributos.group(1));   // nome do atributo
+//                    e.setContTag(atributos.group(2));   // valor do atributo
+////                    e.setContTag(atributos.group(4));
+//                    System.out.println("bateu");
+//                    System.out.println("qtde grupos: " + atributos.groupCount());
+//                    for (int i = 0; i <= atributos.groupCount(); i++) {
+//                        System.out.println("atributos("+i+"): " + atributos.group(i) );
+//                    }
+//                    if (atributos.group(1).equals("year")) {
+//                        Pattern p = Pattern.compile("[{]?(\\d+)[}]?[,]?");
+//                        
+//                        Matcher m = p.matcher(atributos.group(2));
+//                        System.out.println("qtde grupos: " + m.groupCount());
+//                        for (int i = 0; i <= m.groupCount(); i++) {
+//                            System.out.println("m("+i+"): " + m.group(i) );
+//                        }
+//                        
+////                        System.out.println("Achou: " + m.group(0));
+////                        e.setAno(atributos.group(2));
+//                    }
+//                }
+//                
+//            }
+//            
+//        }   catch (IOException ex) {
+//            System.out.println("Ocorreu um erro ao ler o arquivo! " + ex.getMessage());
+//        }
+//        
+//        
+//        
+//        return teste;
+//    }
 }
