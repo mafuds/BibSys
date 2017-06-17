c0ab72554e6a8f413566bcdbdffa2430de140e5e
mariana <marifant26@gmail.com>
2017-06-17 20:30:19 -0300

    Lê uma referencia, precisa arrumar pra ler varias

--- src/br/unesp/rc/bibsys/utils/Conversor.java
+++ src/br/unesp/rc/bibsys/utils/Conversor.java
@@ -8,9 +8,12 @@ package br.unesp.rc.bibsys.utils;
 import br.unesp.rc.bibsys.beans.Elemento;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -37,61 +40,330 @@ public class Conversor
         return conteudo.toString();
     }
     
+    
     static public File converte(File arquivo) {
-        String ref = "";
+        String linha, ref = "", nomeTag = "", valorTag = "";
+        int i, max;
+        char c;
+        Elemento e = new Elemento();
+        HashMap<String, String> hm = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+            linha = reader.readLine();
+            if (linha.matches("[@].*")) {
+                for (i = 0; i < linha.length(); i++) {
+                    c = linha.charAt(i);
+                    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                        ref = ref + c;
+                    }
+                    if (c == '{') {
+                        break;
+                    }
+                }
+                e.setReferencia(ref);
+                
+                linha = reader.readLine();
+                while (linha != null && (!linha.isEmpty() || !linha.matches("\\s"))) {
+                    i = 0;
+                    c = linha.charAt(i);
+                    max = linha.length();
+                    while (!(c >= 'a' && c <= 'z') && !(c >= 'A' && c <= 'Z')) {
+                        i++;
+                        if (i >= max) {
+                            break;
+                        }
+                        c = linha.charAt(i);
+                    }
+                    nomeTag = "";
+                    while ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                        nomeTag = nomeTag + c;
+                        i++;
+                        c = linha.charAt(i);
+                    }
+                    System.out.println("nomeTag: " + nomeTag);
+                    
+                    while (!(c == '=')) {
+                        i++;
+                        if (i >= max) {
+                            break;
+                        }
+                        c = linha.charAt(i);
+                    }
+                    if (c == '=') {
+                        i++;
+                        c = linha.charAt(i);
+                    }
+                    while (c == ' ') {
+                        i++;
+                        if (i >= max) {
+                            break;
+                        }
+                        c = linha.charAt(i);
+                    }
+                    if (c == '{' || c == '"') {
+                        i++;
+                        c = linha.charAt(i);
+                    }
+                    valorTag = "";
+                    while (c != '}' && c != '"' && c != '\r') {
+                        valorTag = valorTag + c;
+                        i++;
+                        if (i >= max) {
+                            break;
+                        }
+                        c = linha.charAt(i);
+                    }
+//                    System.out.println("valor: " + valorTag);
+                    
+                    // se tiver achado nome e valor de tag, salva no hashmap
+                    if (!nomeTag.equals("") && !valorTag.equals("")) {
+                        hm.put(nomeTag, valorTag);
+                        System.out.println("Hashmap <" + nomeTag + ", " + valorTag + ">");
+                    }
+                    
+                    linha = reader.readLine();
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Ocorreu um erro ao ler o arquivo! " + ex.getMessage());
+        }
+                
+        if (!hm.isEmpty()) {
+            e.setValores(hm);
+        }
+        
+        return arquivo;
+    }
+    
+    static public File converteold(File arquivo) {
+        String ref = "", nomeTag = "", conteudoTag = "";
         Elemento e = new Elemento();
         HashMap<String, String> hm = new HashMap<>();
         int i;
+        
         try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
             i = reader.read();
             while (i != -1) {
                 char s = (char) i;
-                System.out.println("antes do switch: i: " + i + " - s: " + Character.toString ((char) i)); // sendo i o código ascii do caracter lido
-                switch (s) {
-                    // testa se eh a primeira linha (@article, por ex)
-                    case '@':
-                        // lê proximo char
+                while (s != '@') {
+                    i = reader.read();
+                    s = (char) i;
+                }
+                // -------------------------------------------------------------------
+                //  achou o começo da primeira referencia
+                if (s == '@') {
+                    i = reader.read();
+                    s = (char) i;
+                    // le chars e escreve em ref até que acabem as letras
+                    while (i != -1 && ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
+                        ref = ref + s;
                         i = reader.read();
                         s = (char) i;
-                        // le chars e escreve em ref até que acabem as letras
-                        while (i != -1 && ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
-                            ref = ref + s;
-                            i = reader.read();
-                            s = (char) i;
-                        }
-                        e.setReferencia(ref);
-                        break;
-                    case '\r':
-                    case ' ':
-                    case '\n':
-                    case '{':
-//                        System.out.println("entrou no case {");
-                        // ignora o que vem depois do {
-                        while (i != -1 && (s != ',' || s == ' ')) {
-                            // esse while percorre (pra ignorar) até o final da primeira linha
-                            System.out.println("while de dentro " + s);
-                            i = reader.read();
-                            s = (char) i;
-                        }
-//                        i = reader.read();
-//                        s = (char) i;
-                        break;
-//                    case '{':
-//                        break;
-                    default:
+                    }
+                    e.setReferencia(ref);
+                    while (s != '{') {
                         i = reader.read();
-                        break;
+                        s = (char) i;
+                    }
+                    
+                    // -------------------------------------------------------------------
+                    //  achou o abrir chaves da primeira referencia
+                    if (s == '{') {
+                        while (s != '}') {
+                            System.out.println("s: " + s);
+                            // -------------------------------------------------------------------
+                            // enquanto não encontrar string (ou seja, nome do parametro), 
+                            //   ignora o que achar
+                            while ((i != -1) && !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
+                                   ) {
+                                i = reader.read();
+                                s = (char) i;
+                            }
+                            // -------------------------------------------------------------------
+                            //  pega o nome do parametro
+                            nomeTag = "";
+                            while (i != -1 && ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
+                                nomeTag = nomeTag + s;
+                                i = reader.read();
+                                s = (char) i;
+                            }
+
+                            // -------------------------------------------------------------------
+                            //  pula espacos em branco até o =
+                            while ((i != -1) && (s != '=')) {
+                               i = reader.read();
+                               s = (char) i;
+                            }
+                            // passa o igual
+                            i = reader.read();
+                            s = (char) i;
+
+                            // pula espacos em branco ate o começo do valor do parametro
+                            while ((i != -1) && (s != '{' || s != '"') && !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
+                                    && !(i >= '0' && i <= '9')) {
+                                i = reader.read();
+                                s = (char) i;
+                            }
+                            // caso seja { ou ", pula esse char
+                            if (s == '{' || s == '"') {
+                                i = reader.read();
+                                s = (char) i;
+                            }
+
+                            // -------------------------------------------------------------------
+                            //  pega o valor do parametro
+                            conteudoTag = "";
+                            while ((i != -1) && (s != '\r') && (s != '}') && (s != '"')) {
+                                conteudoTag = conteudoTag + s;
+                                i = reader.read();
+                                s = (char) i;
+                            }
+                            // se tiver achado nome e valor de tag, salva no hashmap
+                            if (!nomeTag.equals("") && !conteudoTag.equals("")) {
+                                hm.put(nomeTag, conteudoTag);
+                                System.out.println("Hashmap <" + nomeTag + ", " + conteudoTag + ">");
+                            }
+                        }
+                    }
                 }
-//                i = reader.read();
             }
-        }   catch (IOException ex) {
+        } catch (IOException ex) {
             System.out.println("Ocorreu um erro ao ler o arquivo! " + ex.getMessage());
+
+        }
+                
+        if (!hm.isEmpty()) {
+            e.setValores(hm);
         }
 
-        System.out.println("ref: " + e.getReferencia());
+//        for (String key : hm.keySet()) {
+//            //Capturamos o valor a partir da chave
+//            String value = hm.get(key);
+//            System.out.println(key + " = " + value);
+//        }
+        
+//        System.out.println("ref: " + e.getReferencia());
         
         return arquivo;
     }
+    
+    
+    
+//    static public File converte(File arquivo) {
+//        String ref = "", nomeTag = "", conteudoTag = "";
+//        Elemento e = new Elemento();
+//        HashMap<String, String> hm = new HashMap<>();
+//        int i;
+//        
+//        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+//            i = reader.read();
+//            while (i != -1) {
+//                char s = (char) i;
+////                System.out.println("antes do switch: i: " + i + " - s: " + Character.toString ((char) i)); // sendo i o código ascii do caracter lido
+//                switch (s) {
+//                    // testa se eh a primeira linha (@article, por ex)
+//                    case '@':
+//                        // lê proximo char
+//                        i = reader.read();
+//                        s = (char) i;
+//                        // le chars e escreve em ref até que acabem as letras
+//                        while (i != -1 && ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
+//                            ref = ref + s;
+//                            i = reader.read();
+//                            s = (char) i;
+//                        }
+//                        e.setReferencia(ref);
+//                        break;
+//                    case '\r':
+//                    case ' ':
+//                    case '\n':
+//                    case '{':
+//                        while (i != -1 && s != '}') {
+//                            // -------------------------------------------------------------------
+//                            //      começa a leitura de atributos da referencia
+//                            // -------------------------------------------------------------------
+//                            // ignora tudo o que está depois do primeiro abrir chaves
+//                            // vai ate o final da primeira linha
+//                            while ((i != -1) && (s != '\r')) { 
+//                                i = reader.read();
+//                                s = (char) i;
+//                            }
+//                            // -------------------------------------------------------------------
+//                            //  pula espacos em branco ate chegar ao nome do primeiro parametro
+//                            while ((i != -1) && !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
+//                               i = reader.read();
+//                               s = (char) i;
+//                            }
+//
+//                            // -------------------------------------------------------------------
+//                            //  pega o nome do parametro
+//                            nomeTag = "";
+//                            while (i != -1 && ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))) {
+//                                nomeTag = nomeTag + s;
+//                                i = reader.read();
+//                                s = (char) i;
+//                            }
+//    //                        System.out.println("nome tag: " + nomeTag);
+//
+//                            // -------------------------------------------------------------------
+//                            //  pula espacos em branco até o =
+//                            while ((i != -1) && (s != '=')) {
+//                               i = reader.read();
+//                               s = (char) i;
+//                            }
+//                            // passa o igual
+//                            i = reader.read();
+//                            s = (char) i;
+//
+//                            // pula espacos em branco ate o começo do valor do parametro
+//    //                        while ((i != -1) && (s == ' ')) {
+//                            while ((i != -1) && (s != '{' || s != '"') && !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
+//                                    && !(i >= '0' && i <= '9')) {
+//                                i = reader.read();
+//                                s = (char) i;
+//                            }
+//                            if (s == '{' || s == '"') {
+//                                i = reader.read();
+//                                s = (char) i;
+//                            }
+//
+//                            // -------------------------------------------------------------------
+//                            //  pega o valor do parametro
+//                            conteudoTag = "";
+//                            while ((i != -1) && (s != '\r') && (s != '}') && (s != '"')) {
+//                                conteudoTag = conteudoTag + s;
+//                                i = reader.read();
+//                                s = (char) i;
+//                            }
+//    //                        System.out.println("conteudo tag: " + conteudoTag);
+//                            if (!nomeTag.equals("") && !conteudoTag.equals("")) {
+//                                hm.put(nomeTag, conteudoTag);
+//                                System.out.println("Hashmap <" + nomeTag + ", " + conteudoTag + ">");
+//                            }
+//                        }
+//                        break;
+//                    default:
+//                        i = reader.read();
+//                        break;
+//                }
+////                i = reader.read();
+//            }
+//        }   catch (IOException ex) {
+//            System.out.println("Ocorreu um erro ao ler o arquivo! " + ex.getMessage());
+//        }
+//        if (!hm.isEmpty()) {
+//            e.setValores(hm);
+//        }
+//
+////        for (String key : hm.keySet()) {
+////            //Capturamos o valor a partir da chave
+////            String value = hm.get(key);
+////            System.out.println(key + " = " + value);
+////        }
+//        
+////        System.out.println("ref: " + e.getReferencia());
+//        
+//        return arquivo;
+//    }
     
 //    static public File converte(File arquivo) {
 //        System.out.println("entrou no converte");
